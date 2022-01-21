f1bdd6e47f896e6423cb432541da918886aeedb5
Lapinas <mrlapinas99@gmail.com>
2022-01-21 16:29:27 +0000

    setup WeatherFetch.js

--- src/components/WeatherFetch.js
+++ src/components/WeatherFetch.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const WeatherFetch = () => {
+    // Our API Key & Hooks
+    // Declare hooks that will be assigned appropriate values when we get data from the OpenWeatherMap Server.
+    const key = "cec75cfdda440d2f5958a9411cccb517";
+    const [feels_like, setFeelsLike] = useState("");
+    const [mainTemp, setMainTemp] = useState("");
+    const [description, setDescription] = useState("");
+    const [main, setMain] = useState("");
+    const [iconID, setIconID] = useState("");
+
+
+    // useEffect() function which will run when the component is mounted for the first time on the web page.
+    // Will be adding more to it later on...
+    useEffect(() => {
+
+    }, []);
+    
   return (
       <div>
-          
+
       </div>
   )
 }
